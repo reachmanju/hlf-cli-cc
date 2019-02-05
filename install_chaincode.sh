4a12663754a7151d28ad6bfb77259a10d797cbf1
reachmanju <42078329+reachmanju@users.noreply.github.com>
2019-02-05 00:54:08 -0600

    Add files via upload

---
+++ install_chaincode.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+if [ -d "${PWD}/configFiles" ]; then
+    KUBECONFIG_FOLDER=${PWD}/configFiles
+else
+    echo "Configuration files are not found."
+    exit
+fi
+
+# Creating Persistant Volume
+echo -e "\nCreating volume"
+if [ "$(kubectl get pvc | grep shared-pvc | awk '{print $2}')" != "Bound" ]; then
+    echo "The Persistant Volume does not seem to exist or is not bound"
+    echo "Creating Persistant Volume"
+
+    echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/createVolume.yaml"
+    kubectl create -f ${KUBECONFIG_FOLDER}/createVolume.yaml
+    sleep 5
+
+    if [ "kubectl get pvc | grep shared-pvc | awk '{print $3}'" != "shared-pv" ]; then
+        echo "Success creating Persistant Volume"
+    else
+        echo "Failed to create Persistant Volume"
+    fi
+else
+    echo "The Persistant Volume exists, not creating again"
+fi
+
+# Copy the required files(configtx.yaml, cruypto-config.yaml, sample chaincode etc.) into volume
+echo -e "\nCreating Copy artifacts job."
+echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml"
+kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
+
+pod=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
+
+podSTATUS=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
+
+while [ "${podSTATUS}" != "Running" ]; do
+    echo "Wating for container of copy artifact pod to run. Current status of ${pod} is ${podSTATUS}"
+    sleep 5;
+    if [ "${podSTATUS}" == "Error" ]; then
+        echo "There is an error in copyartifacts job. Please check logs."
+        exit 1
+    fi
+    podSTATUS=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
+done
+
+echo -e "${pod} is now ${podSTATUS}"
+echo -e "\nStarting to copy artifacts in persistent volume."
+
+#fix for this script to work on icp and ICS
+kubectl cp ./artifacts $pod:/shared/
+kubectl cp ./crypto-config $pod:/shared/
+
+echo "Waiting for 10 more seconds for copying artifacts to avoid any network delay"
+sleep 10
+JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for copyartifacts job to complete"
+    sleep 1;
+    PODSTATUS=$(kubectl get pods | grep "copyartifacts" | awk '{print $3}')
+        if [ "${PODSTATUS}" == "Error" ]; then
+            echo "There is an error in copyartifacts job. Please check logs."
+            exit 1
+        fi
+    JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
+done
+echo "Copy artifacts job completed"
+
+
+# Install chaincode on each peer
+echo -e "\nCreating installchaincode job"
+echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml"
+kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml
+
+JOBSTATUS=$(kubectl get jobs |grep chaincodeinstall |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for chaincodeinstall job to be completed"
+    sleep 1;
+    if [ "$(kubectl get pods | grep chaincodeinstall | awk '{print $3}')" == "Error" ]; then
+        echo "Chaincode Install Failed"
+        exit 1
+    fi
+    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstall |awk '{print $3}')
+done
+echo "Chaincode Install Completed Successfully"
+
+
+# Instantiate chaincode on channel
+echo -e "\nCreating chaincodeinstantiate job"
+echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
+kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
+
+JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for chaincodeinstantiate job to be completed"
+    sleep 1;
+    if [ "$(kubectl get pods | grep chaincodeinstantiate | awk '{print $3}')" == "Error" ]; then
+        echo "Chaincode Instantiation Failed"
+        exit 1
+    fi
+    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $3}')
+done
+echo "Chaincode Instantiation Completed Successfully"
+
+sleep 15
+echo -e "\nNetwork Setup Completed !!"
